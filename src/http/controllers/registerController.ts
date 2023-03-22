2d9034a5080d4a26a2ab14fac5ef0c9bf33c8e50
alecastrovisk <alexandre.acrsf@gmail.com>
2023-03-22 10:57:52 -0300

    Editando nomenclatura

--- src/http/controllers/registerController.ts
+++ src/http/controllers/registerController.ts
@@ -16,8 +16,8 @@ export async function registerController(
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
 
     await registerUseCase.execute({
       name,
