010b877cd93a44187949f314c64a63e58b04ac32
alecastrovisk <alexandre.acrsf@gmail.com>
2023-03-27 12:09:13 -0300

    Controller e Rota de autenticação

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
+import { authenticateController } from './controllers/authenticateController'
 import { registerController } from './controllers/registerController'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerController)
+
+  app.post('/sessions', authenticateController)
 }
---
+++ src/use-cases/authenticate.spec.ts
@@ -0,0 +1,55 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { describe, it, expect } from 'vitest'
+import { AuthenticateUseCase } from './authenticate'
+import { InvalidCredentialError } from './errors/invalid-credentials-error'
+
+describe('Authenticate Use Case', () => {
+  it('should be able to authenticate', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await usersRepository.create({
+      name: 'User One',
+      email: 'user@email.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'user@email.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    expect(async () =>
+      sut.execute({
+        email: 'user@email.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await usersRepository.create({
+      name: 'User One',
+      email: 'user@email.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    expect(async () =>
+      sut.execute({
+        email: 'user@email.com',
+        password: '123455',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialError)
+  })
+})
